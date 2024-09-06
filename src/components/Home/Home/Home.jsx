5c3ec14dcc821cad9d6a666648b6fad52ab8150b
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-09-06 22:47:16 +0600

    responsive ourApp section

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -5,6 +5,7 @@ import LoadingPage from '../../LoadingPage/LoadingPage';
 import Banner from '../Banner/Banner';
 import Facilities from '../Facilities/Facilities';
 import FeaturedBooks from '../FeaturedBooks/FeaturedBooks';
+import OurApp from '../OurApp/OurApp';
 
 
 const Home = () => {
@@ -23,7 +24,9 @@ const Home = () => {
       {/* featuredBooks section start */}
       <FeaturedBooks></FeaturedBooks>
       {/* featuredBooks section end */}
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui distinctio cumque a necessitatibus, ex labore. Maiores id accusantium nihil delectus excepturi, eum voluptatum asperiores debitis, praesentium assumenda voluptatem, exercitationem eius quis repudiandae vero tempore modi voluptate dignissimos est. Dolore vero nesciunt necessitatibus odit error iste reiciendis quos, vitae rem voluptas facilis laborum consequuntur totam veritatis commodi natus ea distinctio voluptatibus tenetur, nam perspiciatis soluta beatae, officia quisquam? Enim pariatur incidunt culpa alias magni ea, ab rerum fuga accusantium quibusdam repudiandae nihil reprehenderit cumque iste. Deserunt quas doloribus dolore modi, vitae vero quo sunt necessitatibus rerum eligendi omnis nihil iure eos sequi commodi? Assumenda, deserunt possimus qui repellendus totam, delectus corporis dolore accusamus mollitia veniam iste voluptatum cupiditate necessitatibus temporibus nemo unde fugiat exercitationem. Modi delectus maxime et praesentium sequi? Id corrupti aut obcaecati laudantium autem accusantium molestias? Ab saepe nihil deserunt error praesentium. Autem sit maiores at accusamus quas. Neque omnis voluptas deleniti laboriosam non eveniet molestiae laborum explicabo expedita quia voluptatum culpa consectetur quos consequatur, saepe quasi. Tenetur iste ullam voluptatum ipsam hic, totam magni, nam aspernatur, natus necessitatibus dignissimos reiciendis recusandae iusto animi omnis culpa similique delectus facilis? Quam cum natus commodi reprehenderit ipsam ullam, sed sunt nostrum?</p>
+      {/* outApp section start */}
+      <OurApp></OurApp>
+      {/* outApp section end */}
     </div>
   );
 };
---
+++ src/components/Home/OurApp/OurApp.jsx
@@ -0,0 +1,48 @@
+import { FaApple } from "react-icons/fa";
+import { AiFillAndroid } from "react-icons/ai";
+
+
+const OurApp = () => {
+  return (
+    <div className="bg-yellow-400 py-14">
+      <div className="container mx-auto px-5">
+        {/* content section start */}
+        <div className="text-center space-y-5">
+          {/* title section start */}
+          <h3 className="text-blue-500 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-4xl font-bold">Download Our App For All Platform</h3>
+          <p className="text-sm sm:text-sm md:text-[15px] lg:text-base xl:text-lg 2xl:text-xl text-gray-500 text-justify md:text-center">It won’t be a bigger problem to find one video game lover in your neighbor. Since the introduction of <br className="hidden md:block"></br>Virtual Game, it has been achieving great heights so far as its popularity <br className="hidden md:block"></br>and technological advancement are concerned.</p>
+          {/* title section end */}
+          {/* app section start */}
+          <div className="flex justify-center gap-3 md:gap-4 xl:gap-5 2xl:gap-7">
+            {/* item1 section start */}
+            <ul className="flex justify-center items-center gap-3 bg-gray-500 w-1/2 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 2xl:1/5 h-16 md:h-20 lg:h-20 xl:h-24 2xl:h-24">
+              <li className="text-blue-700">
+                <FaApple className="text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-4xl"></FaApple>
+              </li>
+              <li>
+                <span className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold text-black">Apple Store</span><br></br>
+                <span className="text-[10px] sm:text-xs md:text-xs lg:text-sm xl:text-sm 2xl:text-lg text-white italic">on App Store</span>
+              </li>
+            </ul>
+            {/* item1 section end */}
+            {/* item2 section start */}
+            <ul className="flex justify-center items-center gap-3 bg-gray-500 w-1/2 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-1/5 2xl:1/5 h-16 md:h-20 lg:h-20 xl:h-24 2xl:h-24">
+              <li className="text-blue-700">
+                <AiFillAndroid className="text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-4xl"></AiFillAndroid>
+              </li>
+              <li>
+                <span className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold text-black">Android Store</span><br></br>
+                <span className="text-[10px] sm:text-xs md:text-xs lg:text-sm xl:text-sm 2xl:text-lg text-white italic">on App Store</span>
+              </li>
+            </ul>
+            {/* item2 section end */}
+          </div>
+          {/* app section end */}
+        </div>
+        {/* content section end */}
+      </div>
+    </div>
+  );
+};
+
+export default OurApp;
