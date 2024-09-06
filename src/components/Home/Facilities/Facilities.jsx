bcd3d363b3f246316eebecb20825b6cb458d7bf4
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-09-06 18:01:47 +0600

    create facilities section in homepage

---
+++ src/components/Home/Facilities/Facilities.jsx
@@ -0,0 +1,62 @@
+import { FaTruckFast, FaClockRotateLeft } from "react-icons/fa6";
+import { FaLock,  FaHeadset } from "react-icons/fa";
+
+
+const Facilities = () => {
+  return (
+    <div className="bg-blue-700 py-5">
+      <div className="container mx-auto px-5">
+        {/* content section start */}
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-3 sm:gap-2 xl:gap-5">
+          {/* item1 start */}
+          <ul className="flex justify-center items-center gap-3">
+            <li className="text-yellow-500">
+              <FaTruckFast className="text-lg sm:text-[19px] md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl"></FaTruckFast>
+            </li>
+            <li>
+              <span className="text-[13px] md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold text-black">Free Shipping</span><br></br>
+              <span className="text-[10px] md:text-xs lg:text-sm xl:text-sm 2xl:text-base text-white italic">Order Over $100</span>
+            </li>
+          </ul>
+          {/* item1 end */}
+          {/* item2 start */}
+          <ul className="flex justify-center items-center gap-3">
+            <li className="text-yellow-500">
+              <FaLock className="text-lg sm:text-[19px] md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl"></FaLock>
+            </li>
+            <li>
+              <span className="text-[13px] md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold text-black">Secure Payment</span><br></br>
+              <span className="text-[10px] md:text-xs lg:text-sm xl:text-sm 2xl:text-base text-white italic">100% Secure Payment</span>
+            </li>
+          </ul>
+          {/* item2 end */}
+          {/* item3 start */}
+          <ul className="flex justify-center items-center gap-3">
+            <li className="text-yellow-500">
+              <FaClockRotateLeft className="text-lg sm:text-[19px] md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl"></FaClockRotateLeft>
+            </li>
+            <li>
+              <span className="text-[13px] md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold text-black">Easy Returns</span><br></br>
+              <span className="text-[10px] md:text-xs lg:text-sm xl:text-sm 2xl:text-base text-white italic">10 Days Returns</span>
+            </li>
+          </ul>
+          {/* item3 end */}
+          {/* item4 start */}
+          <ul className="flex justify-center items-center gap-3">
+            <li className="text-yellow-500">
+              <FaHeadset className="text-lg sm:text-[19px] md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl"></FaHeadset>
+            </li>
+            <li>
+              <span className="text-[13px] md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold text-black">24/7 Support</span><br></br>
+              <span className="text-[10px] md:text-xs lg:text-sm xl:text-sm 2xl:text-base text-white italic">Call Us Anytime</span>
+            </li>
+          </ul>
+          {/* item4 end */}
+        </div>
+        {/* content section end */}
+      </div>
+    </div>
+  );
+};
+
+export default Facilities;
--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import { useNavigation } from 'react-router-dom';
 import LoadingPage from '../../LoadingPage/LoadingPage';
 import Banner from '../Banner/Banner';
+import Facilities from '../Facilities/Facilities';
 
 
 const Home = () => {
@@ -15,6 +16,10 @@ const Home = () => {
       {/* banner section start */}
       <Banner></Banner>
       {/* banner section end */}
+      {/* facilities section start */}
+      <Facilities></Facilities>
+      {/* facilities section end */}
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui distinctio cumque a necessitatibus, ex labore. Maiores id accusantium nihil delectus excepturi, eum voluptatum asperiores debitis, praesentium assumenda voluptatem, exercitationem eius quis repudiandae vero tempore modi voluptate dignissimos est. Dolore vero nesciunt necessitatibus odit error iste reiciendis quos, vitae rem voluptas facilis laborum consequuntur totam veritatis commodi natus ea distinctio voluptatibus tenetur, nam perspiciatis soluta beatae, officia quisquam? Enim pariatur incidunt culpa alias magni ea, ab rerum fuga accusantium quibusdam repudiandae nihil reprehenderit cumque iste. Deserunt quas doloribus dolore modi, vitae vero quo sunt necessitatibus rerum eligendi omnis nihil iure eos sequi commodi? Assumenda, deserunt possimus qui repellendus totam, delectus corporis dolore accusamus mollitia veniam iste voluptatum cupiditate necessitatibus temporibus nemo unde fugiat exercitationem. Modi delectus maxime et praesentium sequi? Id corrupti aut obcaecati laudantium autem accusantium molestias? Ab saepe nihil deserunt error praesentium. Autem sit maiores at accusamus quas. Neque omnis voluptas deleniti laboriosam non eveniet molestiae laborum explicabo expedita quia voluptatum culpa consectetur quos consequatur, saepe quasi. Tenetur iste ullam voluptatum ipsam hic, totam magni, nam aspernatur, natus necessitatibus dignissimos reiciendis recusandae iusto animi omnis culpa similique delectus facilis? Quam cum natus commodi reprehenderit ipsam ullam, sed sunt nostrum?</p>
     </div>
   );
 };
